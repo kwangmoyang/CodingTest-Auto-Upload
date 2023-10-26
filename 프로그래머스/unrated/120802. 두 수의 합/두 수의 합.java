938505a5672da9b681795bad1e7b77c31fb27dec
Hikari <100852794+kwangmoyang@users.noreply.github.com>
2023-10-26 10:18:20 +0900

    [level unrated] Title: 두 수의 합, Time: 0.03 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if(num1>=-50000&&num1<=50000 && num2>=-50000 && num2<=50000){
+            answer = num1+num2;
+        }
+            return answer;
+    }
+}
