ba04a5e0e6e3b9429fdaf0223e260aa46c5c5b88
Hikari <100852794+kwangmoyang@users.noreply.github.com>
2024-02-18 11:22:16 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+ 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		in.close();
+		
+		System.out.print((A>=90)?"A": (A>=80)? "B": (A>=70)? "C": (A>=60)? "D": "F");
+	}
+}
