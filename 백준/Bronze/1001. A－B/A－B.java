26b96ba184f7ad10e04d4bf54f0b9e0e98d42b26
Hikari <100852794+kwangmoyang@users.noreply.github.com>
2024-02-18 11:19:38 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        
+        System.out.println(num1-num2);
+    }
+}
